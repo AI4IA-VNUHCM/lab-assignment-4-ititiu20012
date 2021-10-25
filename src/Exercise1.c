333e9e0981371efc23fe833eada05f399a699c24
ititiu20012 <ititiu20012@student.hcmiu.edu.vn>
2021-10-25 14:12:03 +0700

    Ex1

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -17,7 +17,19 @@ ______________________________________
 
 void Ex1(int n){
 	//Your codes here
-    
+    int space,c,j,i;
+	for ( i=0;i<=n;i++){
+		for ( j =0; j <=i;j++){
+			if ( i==0 || j== 0){
+				c =1;
+			}
+			else {
+				c= (c*( i-j+1))/j;
+			}
+			printf("%d ",c);
+		}
+		printf("\n");
+	}
 }
 
 int main(int argc, char *argv[]) {
